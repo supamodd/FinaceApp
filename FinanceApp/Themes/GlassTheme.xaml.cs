65d4d1dbdad04717605ac87f24823d95a32ef61a
supamodd <ghosts.zz@yandex.ru>
2026-05-21 11:05:50 +0300

    add theme glass

---
+++ FinanceApp/Themes/GlassTheme.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinanceApp.Themes
+{
+    /// <summary>
+    /// Interaction logic for GlassTheme.xaml
+    /// </summary>
+    public partial class GlassTheme : Page
+    {
+        public GlassTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
